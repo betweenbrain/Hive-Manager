cae71dc3fc56196737b85d384e51afb9b31486b4
Matt Thomas <matt@betweenbrain.com>
2014-08-28 16:23:49 -0400

    Make hives view default

--- admin/controller.php
+++ admin/controller.php
@@ -16,4 +16,12 @@
  */
 class HivemanagerController extends JControllerLegacy
 {
+
+	/**
+	 * The default view for the display method.
+	 *
+	 * @var string
+	 * @since 12.2
+	 */
+	protected $default_view = 'Hives';
 }
