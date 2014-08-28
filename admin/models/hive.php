2183e26ac7f44cb4e8e17cff55c7de1f249ba60e
Matt Thomas <matt@betweenbrain.com>
2014-08-28 14:40:53 -0400

    Adds the foundation of editing a hive entry

---
+++ admin/models/hive.php
@@ -0,0 +1,70 @@
+<?php defined('_JEXEC') or die;
+
+/**
+ * File       hive.php
+ * Created    8/28/14 1:39 PM
+ * Author     Matt Thomas | matt@betweenbrain.com | http://betweenbrain.com
+ * Support    https://github.com/betweenbrain/
+ * Copyright  Copyright (C) 2014 betweenbrain llc. All Rights Reserved.
+ * License    GNU GPL v2 or later
+ */
+
+jimport('joomla.application.component.modelform');
+
+class HivemanagerModelHive extends JModelForm
+{
+
+	/**
+	 * Returns a reference to the a Table object, always creating it.
+	 *
+	 * @param       type    The table type to instantiate
+	 * @param       string  A prefix for the table class name. Optional.
+	 * @param       array   Configuration array for model. Optional.
+	 *
+	 * @return      JTable  A database object
+	 * @since       2.5
+	 */
+	public function getTable($type = 'Hives', $prefix = 'HivemanagerTable', $config = array())
+	{
+		return JTable::getInstance($type, $prefix, $config);
+	}
+
+	/**
+	 * Method to get the record form.
+	 *
+	 * @param       array   $data     Data for the form.
+	 * @param       boolean $loadData True if the form is to load its own data (default case), false if not.
+	 *
+	 * @return      mixed   A JForm object on success, false on failure
+	 * @since       2.5
+	 */
+	public function getForm($data = array(), $loadData = true)
+	{
+		// Get the form.
+		$form = $this->loadForm('com_hivemanager.hive', 'hive', array('control' => 'jform', 'load_data' => $loadData));
+
+		if (empty($form))
+		{
+			return false;
+		}
+
+		return $form;
+	}
+
+	/**
+	 * Method to get the data that should be injected in the form.
+	 *
+	 * @return      mixed   The data for the form.
+	 * @since       2.5
+	 */
+	protected function loadFormData()
+	{
+		// Check the session for previously entered form data.
+		$data = JFactory::getApplication()->getUserState('com_hivemanager.edit.hive.data', array());
+
+		if (empty($data))
+		{
+			return $data;
+		}
+	}
+}
