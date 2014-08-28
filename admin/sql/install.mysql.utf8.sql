7a560b5421c2a3e63f36f1a362a2ab9d100699a8
Matt Thomas <matt@betweenbrain.com>
2014-08-28 15:29:08 -0400

    Basic install / uninstall queries

---
+++ admin/sql/install.mysql.utf8.sql
@@ -0,0 +1,12 @@
+-- <?php defined('_JEXEC') or die ?>;
+
+CREATE TABLE IF NOT EXISTS `#__hivemanager_hives` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL DEFAULT '',
+  `type` varchar (100) NOT NULL,
+  `lat` DECIMAL(10, 8) NOT NULL,
+  `lng` DECIMAL(11, 8) NOT NULL,
+  `size` INT(1) NOT NULL,
+  `started` datetime NOT NULL DEFAULT '0000-00-00',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
