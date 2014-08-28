c8e2613423c2beaede780b31d4f2b132c9b7dd19
Matt Thomas <matt@betweenbrain.com>
2014-08-28 16:21:09 -0400

    Adds controller to use JControllerForm::save()

---
+++ admin/controllers/hive.php
@@ -0,0 +1,21 @@
+<?php defined('_JEXEC') or die;
+
+/**
+ * File       hive.php
+ * Created    8/28/14 3:22 PM
+ * Author     Matt Thomas | matt@betweenbrain.com | http://betweenbrain.com
+ * Support    https://github.com/betweenbrain/
+ * Copyright  Copyright (C) 2014 betweenbrain llc. All Rights Reserved.
+ * License    GNU GPL v2 or later
+ */
+
+// import Joomla controlleradmin library
+jimport('joomla.application.component.controlleradmin');
+
+/**
+ * HelloWorlds Controller
+ */
+class HivemanagerControllerHive extends JControllerForm
+{
+
+}
