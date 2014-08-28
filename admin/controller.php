86a2c9fd439d5a84e5c4b3c24e4ecf0767b83dc3
Matt Thomas <matt@betweenbrain.com>
2014-08-28 16:23:26 -0400

    Adds empty controller

---
+++ admin/controller.php
@@ -0,0 +1,19 @@
+<?php defined('_JEXEC') or die;
+
+/**
+ * File       controller.php
+ * Created    8/28/14 1:10 PM
+ * Author     Matt Thomas | matt@betweenbrain.com | http://betweenbrain.com
+ * Support    https://github.com/betweenbrain/
+ * Copyright  Copyright (C) 2014 betweenbrain llc. All Rights Reserved.
+ * License    GNU GPL v2 or later
+ */
+
+/**
+ * General Controller of Hive Manager component
+ *
+ * @since  0.1
+ */
+class HivemanagerController extends JControllerLegacy
+{
+}
