90e217dbed4babe0edb31986e67339bee7f6bf9f
Matt Thomas <matt@betweenbrain.com>
2014-08-29 10:53:24 -0400

    Basic admin list of hives view

--- admin/views/hives/tmpl/default.php
+++ admin/views/hives/tmpl/default.php
@@ -2,14 +2,75 @@
 
 /**
  * File       default.php
- * Created    8/28/14 4:08 PM 
+ * Created    8/28/14 4:08 PM
  * Author     Matt Thomas | matt@betweenbrain.com | http://betweenbrain.com
  * Support    https://github.com/betweenbrain/
  * Copyright  Copyright (C) 2014 betweenbrain llc. All Rights Reserved.
  * License    GNU GPL v2 or later
  */
+?>
 
-foreach ($this->items as $item)
-{
-	echo '<pre>' . print_r($item, true) . '</pre>';
-}
+<form action="<?php echo JRoute::_('index.php?option=com_content&view=articles'); ?>" method="post" name="adminForm" id="adminForm">
+	<div id="j-main-container">
+		<table class="table table-striped" id="hives-list">
+			<thead>
+			<tr>
+				<th width="1%" class="nowrap center">
+					<?php echo JText::_('COM_HIVEMANAGER_ID'); ?>
+				</th>
+				<th class="nowrap">
+					<?php echo JText::_('COM_HIVEMANAGER_NAME'); ?>
+				</th>
+				<th class="nowrap">
+					<?php echo JText::_('COM_HIVEMANAGER_TYPE'); ?>
+				</th>
+				<th class="nowrap">
+					<?php echo JText::_('COM_HIVEMANAGER_LAT'); ?>
+				</th>
+				<th class="nowrap">
+					<?php echo JText::_('COM_HIVEMANAGER_LNG'); ?>
+				</th>
+				<th class="nowrap">
+					<?php echo JText::_('COM_HIVEMANAGER_SIZE'); ?>
+				</th>
+				<th class="nowrap">
+					<?php echo JText::_('COM_HIVEMANAGER_STARTED'); ?>
+				</th>
+			</tr>
+			</thead>
+			<tbody>
+			<?php foreach ($this->hives as $i => $hive) : ?>
+				<tr class="row<?php echo $i % 2; ?>">
+					<td class="center">
+						<?php echo JHtml::_('grid.id', $i, $hive->id); ?>
+					</td>
+					<td class="center">
+						<?php echo $hive->id; ?>
+					</td>
+					<td>
+						<?php echo $hive->name; ?>
+					</td>
+					<td>
+						<?php echo $hive->type; ?>
+					</td>
+					<td>
+						<?php echo $hive->lat; ?>
+					</td>
+					<td>
+						<?php echo $hive->lng; ?>
+					</td>
+					<td>
+						<?php echo $hive->size; ?>
+					</td>
+					<td>
+						<?php echo $hive->started; ?>
+					</td>
+				</tr>
+			<?php endforeach; ?>
+			</tbody>
+		</table>
+		<input type="hidden" name="task" value="" />
+		<input type="hidden" name="boxchecked" value="0" />
+		<?php echo JHtml::_('form.token'); ?>
+	</div>
+</form>
